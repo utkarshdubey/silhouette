01ea12f71aca1c230e3c28c85e40e6596a2a6071
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-07-10 12:13:15 +0530

    Added rounded SVG support.

--- src/generate.ts
+++ src/generate.ts
@@ -15,18 +15,21 @@ export function generateAvatar(text: string, options: any) {
         svgOptions = {
             backgroundColor: options.backgroundColor ? options.backgroundColor : randomColor,
             textColor: options.backgroundColor ? generateTextColor(options.backgroundColor) : generateTextColor(randomColor),
-            font: "sans-serif"
+            font: "sans-serif",
+            rounded: options.rounded ? true : false
         }
     } else{
         svgOptions = {
             backgroundColor: randomColor,
             textColor: generateTextColor(randomColor),
-            font: "sans-serif"
+            font: "sans-serif",
+            rounded: false
         }
     }
 
     // SVG Code
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 746.49 836.37"><path fill="${svgOptions.backgroundColor || "#d8bfdc"}" d="M0 0h746.5v836.37H0z"/><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="428.57" fill="${svgOptions.textColor || "#231f20"}" font-family="${svgOptions.font}">${initials}</text></svg>`
+    console.log(svgOptions)
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${svgOptions.rounded ? "0 0 1000 1000" : "0 0 746.49 836.37"}">${svgOptions.rounded ? `<circle cx="500" cy="500" r="500" fill="${svgOptions.backgroundColor}" />` : `<path fill="${svgOptions.backgroundColor || "#d8bfdc"}" d="M0 0h746.5v836.37H0z"/>`} <text x="50%" y="${svgOptions.rounded ? "55%" : "50%"}" dominant-baseline="middle" text-anchor="middle" font-size="428.57" fill="${svgOptions.textColor || "#231f20"}" font-family="${svgOptions.font}">${initials}</text></svg>`
 
     return "data:image/svg+xml," + encodeURIComponent(svg);
 }
